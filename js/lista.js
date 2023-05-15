98668b2213a24fe6e1abe7ddb08215b36ddb8b84
Lara Sousa <sousalara822@gmail.com>
2023-05-15 20:39:50 -0300

    Armazenando itens no Local Storage

--- js/lista.js
+++ js/lista.js
@@ -1,28 +1,33 @@
 window.onload = () => {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', adicionarProduto);
+    const form = document.querySelector('.form');
+    const listaProdutos = document.getElementById('lista-produtos');
 
-    function adicionarProduto(event) {
+    form.addEventListener('submit', (event) => {
         event.preventDefault();
 
         // pegar os valores do formulário
-        const nome = document.getElementById('nome').value;
-        const preco = document.getElementById('preco').value;
-        const qtd = document.getElementById('qtd').value;
+        let nome = document.getElementById('nome').value;
+        let preco = document.getElementById('preco').value;
+        let qtd = document.getElementById('qtd').value;
 
         // criar um novo elemento li
         const li = document.createElement('li');
-        li.innerHTML = `${nome} <br> R$:${preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} - ${qtd} unidades`;
+        li.innerHTML = `${nome}<br> ${formatarReal(preco)} - ${qtd} unidades`;
 
         // adicionar o novo elemento à lista
-        const listaProdutos = document.getElementById('lista-produtos');
         listaProdutos.appendChild(li);
+
+        // salvar a lista na localStorage
+        localStorage.setItem('listaProdutos', listaProdutos.innerHTML);
 
         // limpar os campos do formulário
         document.getElementById('nome').value = '';
         document.getElementById('preco').value = '';
         document.getElementById('qtd').value = '';
-    }
+    });
+
+    // carregar a lista da localStorage
+    listaProdutos.innerHTML = localStorage.getItem('listaProdutos');
 
     function formatarReal(valor) {
         const valorFormatado = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
